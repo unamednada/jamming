eeaeb2b2b123f799cee3c8da95366e37b6c20968
unamednada <dias.gustavo.dias@gmail.com>
2021-09-23 17:05:21 -0300

    Create, add css and import PlayList. Refactor render of app to fix background image

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,18 +2,17 @@ import './App.css';
 import React from 'react';
 import SearchBar from '../SearchBar/SearchBar';
 import SearchResults from '../SearchResults/SearchResults';
+import PlayList from '../PlayList/PlayList';
 
 function App() {
   return (
-    <div className="App">
-      <div>
-        <h1>Ja<span className="highlight">mmm</span>ing</h1>
-        <div className="App">
-          <SearchBar />
-          <div className="App-playlist">
-            <SearchResults />
-            {/* <!-- Add a Playlist component --> */}
-          </div>
+    <div>
+      <h1>Ja<span className="highlight">mmm</span>ing</h1>
+      <div className="App">
+        <SearchBar />
+        <div className="App-playlist">
+          <SearchResults />
+          <PlayList />
         </div>
       </div>
     </div>
