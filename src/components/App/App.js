724c1dace178325301fc3c1dfef1f408b4477c67
unamednada <dias.gustavo.dias@gmail.com>
2021-09-23 13:04:03 -0300

    Add jsx to app.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,7 +4,16 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <h1>JAMMING</h1>
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          {/* <!-- Add a SearchBar component --> */}
+          <div className="App-playlist">
+            {/* <!-- Add a SearchResults component --> */}
+            {/* <!-- Add a Playlist component --> */}
+          </div>
+        </div>
+      </div>
     </div>
   ); 
 }
