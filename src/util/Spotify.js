3faf147b9b9aea34687b5da58735353c79956b31
unamednada <dias.gustavo.dias@gmail.com>
2021-10-05 10:08:55 -0300

    Add conditional to spotify.js to get user access token

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,9 +1,25 @@
-const userAccessToken = null;
+let userAccessToken = null;
+const clientID = '4d6e372a4368407abc4362c693bc9957';
+const redirectURI = 'http://localhost:3000/';
 
 export const Spotify = {
   getAccessToken() {
     if (userAccessToken) {
       return userAccessToken;
     }
+
+    const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+    const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+
+    if (accessTokenMatch && expiresInMatch) {
+      userAccessToken = accessTokenMatch[1];
+      const expiresIn = +expiresInMatch[1];
+
+      window.setTimeout(() => userAccessToken = '', expiresIn * 1000);
+      window.history.pushState('Access Token', null, '/');
+      return userAccessToken;
+    }
+
+    window.location = `https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`;
   }
 };
